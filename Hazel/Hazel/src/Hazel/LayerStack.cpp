5129fe4816857609be5b710304709b50332a45f3
FMSH <1157702184@qq.com>
2022-06-08 22:57:10 +0800

    laystack bug,layerInsert address error

--- Hazel/Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/Hazel/src/Hazel/LayerStack.cpp
@@ -11,15 +11,20 @@ namespace Hazel {
 	}
 	void LayerStack::PushLayer(Layer* layer) {
 		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+		m_LayerInsert++;
 	}
 	void LayerStack::PushOverlay(Layer* overlay) {
 		m_Layers.emplace_back(overlay);
 	}
 	void LayerStack::PopLayer(Layer* layer) {
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+		auto it = std::find(m_Layers.begin(), m_LayerInsert, layer);
 		if (it != m_Layers.end()) {
 			m_Layers.erase(it);
 			m_LayerInsert--;
+		}
+		it = std::find(m_LayerInsert, m_Layers.end(), layer);
+		if (it != m_Layers.end()) {
+			m_Layers.erase(it);
 		}
 	}
 	void LayerStack::PopOverlay(Layer* overlay) {
